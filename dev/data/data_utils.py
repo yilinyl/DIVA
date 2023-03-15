fix feature normalization bug

dev/data/data_utils.py
@@ -92,22 +92,36 @@ def extract_variant_graph(uprot_pos, chain, chain_res_list, seq2struct_pos, prot
                   'min': torch.tensor(np.nanmin(prot_graph.edata['dist'].numpy())),
                   'max': torch.tensor(np.nanmax(prot_graph.edata['dist'].numpy()))}
 
-    feat_stats = {'mean': torch.tensor(np.nanmean(feat_data)),
-                  'min': torch.tensor(np.nanmin(feat_data)),
-                  'max': torch.tensor(np.nanmax(feat_data))}
+    feat_stats = {'mean': torch.tensor(np.nanmean(feat_data, axis=0)),
+                  'min': torch.tensor(np.nanmin(feat_data, axis=0)),
+                  'max': torch.tensor(np.nanmax(feat_data, axis=0))}
 
     var_feats = torch.tensor(feat_data[list(map(lambda x: x - 1, seq_pos_remain)), :])
-    var_feats = impute_and_normalize(var_feats, feat_stats, normalize=normalize)
+    var_feats, feat_stats = impute_nan(var_feats, feat_stats)
+    var_graph.edata['angle'], angle_stats = impute_nan(var_graph.edata['angle'], feat_stats=angle_stats)
+    var_graph.edata['dist'], dist_stats = impute_nan(var_graph.edata['dist'], feat_stats=dist_stats)
+    if normalize:
+        var_graph.edata['angle'] = normalize_feat(var_graph.edata['angle'], feat_stats=angle_stats)
+        var_graph.edata['dist'] = normalize_feat(var_graph.edata['dist'], feat_stats=dist_stats)
+        var_feats = normalize_feat(var_feats, feat_stats=feat_stats)
+
     var_graph.ndata['feat'] = var_feats
-    var_graph.edata['angle'] = impute_and_normalize(var_graph.edata['angle'], feat_stats=angle_stats, normalize=normalize)
-    var_graph.edata['dist'] = impute_and_normalize(var_graph.edata['dist'], feat_stats=dist_stats, normalize=normalize)
     var_graph.edata['coev'] = torch.tensor(coev_feats)
     var_graph.edata['feat'] = torch.cat([var_graph.edata['angle'], var_graph.edata['dist'], var_graph.edata['coev']], dim=1)
 
     return var_graph, seq_pos_remain
 
 
-def impute_and_normalize(feat_raw, feat_stats=None, normalize=False):
+def impute_nan(feat_raw, feat_stats=None):
+    """
+    Impute missing values with mean (1-dim feature only)
+    Args:
+        feat_raw:
+        feat_stats:
+
+    Returns:
+
+    """
     # target_idx = []
     # for col in feat_cols:
     #     if col in target_cols:
@@ -116,9 +130,9 @@ def impute_and_normalize(feat_raw, feat_stats=None, normalize=False):
     feat_raw = copy.deepcopy(feat_raw)
     if not feat_stats:
         feat_stats = dict()
-        feat_stats['mean'] = torch.tensor(np.nanmean(feat_raw.numpy()))
-        feat_stats['min'] = torch.tensor(np.nanmin(feat_raw.numpy()))
-        feat_stats['max'] = torch.tensor(np.nanmax(feat_raw.numpy()))
+        feat_stats['mean'] = torch.tensor(np.nanmean(feat_raw.numpy(), axis=0))
+        feat_stats['min'] = torch.tensor(np.nanmin(feat_raw.numpy(), axis=0))
+        feat_stats['max'] = torch.tensor(np.nanmax(feat_raw.numpy(), axis=0))
     # for key in feat_stats:
     #     feat_stats[key] = torch.tensor(feat_stats[key]).clone().detach()
 
@@ -126,10 +140,12 @@ def impute_and_normalize(feat_raw, feat_stats=None, normalize=False):
         na_idx = torch.where(torch.isnan(feat_raw))
         feat_raw[na_idx] = torch.take(feat_stats['mean'], na_idx[1])
 
-    if normalize:
-        return (feat_raw - feat_stats['min']) / (feat_stats['max'] - feat_stats['min'])
+    return feat_raw, feat_stats
 
-    return feat_raw
+
+def normalize_feat(feat_raw, feat_stats):
+    feat_raw = copy.deepcopy(feat_raw)
+    return (feat_raw - feat_stats['min']) / (feat_stats['max'] - feat_stats['min'])
 
 
 def load_pio_features(uprot, feat_path):
@@ -190,6 +206,21 @@ def add_coev_edges(df_coev, feat_names, nlargest=20, src_name='pos1', dst_name='
     g_coev = dgl.from_networkx(nx.from_pandas_edgelist(df_top, src_name, dst_name, edge_attr=True).to_directed(),
                                edge_attrs=feat_names)
     return g_coev
+
+
+def load_nsp_feats(uprot, feat_root, exclude, rewrite_pkl=True):
+    if isinstance(feat_root, str):
+        feat_root = Path(feat_root)
+
+    df_nsp = pd.read_pickle(feat_root / f'{uprot}.pkl')
+    rename_dict = dict(zip(df_nsp.columns, [s.strip() for s in df_nsp.columns]))
+    df_nsp = df_nsp.rename(columns=rename_dict)
+
+    if rewrite_pkl:
+        df_nsp.to_pickle(feat_root / f'{uprot}.pkl')
+
+    return df_nsp.drop(columns=exclude).values
+
 
 # Adapted from Dwivedi, Vijay Prakash, and Xavier Bresson.
 # "A generalization of transformer networks to graphs." https://arxiv.org/abs/2012.09699

dev/data/datasets.py
@@ -21,7 +21,7 @@ class VariantGraphDataSet(Dataset):
                  cov_thres=0.5, num_neighbors=10, distance_type='centroid', method='radius', radius=10, df_ires=None, save=False,
                  anno_ires=False, coord_option=None, feat_stats=None, var_db=None, seq2struct_all=None,
                  var_graph_radius=None, seq_dict=None, window_size=None, graph_type='hetero', norm_feat=True,
-                 save_var_graph=False, var_graph_cache='./var_graph_cache', **kwargs):
+                 save_var_graph=False, var_graph_cache='./var_graph_cache', nsp_dir='', **kwargs):
         super(VariantGraphDataSet, self).__init__()
 
         self.data = []
@@ -124,11 +124,16 @@ class VariantGraphDataSet(Dataset):
             # feat_data = load_pio_features(uprot, feat_path)
             # feat_data = normalize_data(feat_data, feat_stats)
             if uprot != uprot_prev:
-                bio_feats = load_expasy_feats(uprot, feat_root, '3dvip_expasy')
-                pssm_feats = load_pssm(uprot, feat_root, '3dvip_pssm')
-                coev_feat_df = load_coev_feats(uprot, feat_root, '3dvip_sca', '3dvip_dca')
+                try:
+                    bio_feats = load_expasy_feats(uprot, feat_root, '3dvip_expasy')
+                    pssm_feats = load_pssm(uprot, feat_root, '3dvip_pssm')
+                    coev_feat_df = load_coev_feats(uprot, feat_root, '3dvip_sca', '3dvip_dca')
+                    nsp_feat = load_nsp_feats(uprot, nsp_dir, exclude=['asa', 'phi', 'psi', 'disorder'])
 
-                feat_data = np.hstack([bio_feats, pssm_feats])
+                    feat_data = np.hstack([bio_feats, pssm_feats, nsp_feat])
+                except (FileNotFoundError, ValueError) as e:
+                    logging.warning(f'{e} in loading feature for {uprot}')
+                    continue
 
             if f_var_graph.exists():
                 with open(f_var_graph, 'rb') as f_pkl:
@@ -170,8 +175,11 @@ class VariantGraphDataSet(Dataset):
                     }
 
                     var_graph = dgl.heterograph(edge_dict)
-                    var_graph.edges['struct'].data['angle'] = impute_and_normalize(struct_g.edata['angle'], angle_stats, normalize=norm_feat)
-                    var_graph.edges['struct'].data['dist'] = impute_and_normalize(struct_g.edata['dist'], dist_stats, normalize=norm_feat)
+                    angle_feat, angle_stats = impute_nan(struct_g.edata['angle'], angle_stats)
+                    dist_feat, dist_stats = impute_nan(struct_g.edata['dist'], dist_stats)
+                    if norm_feat:
+                        var_graph.edges['struct'].data['angle'] = normalize_feat(angle_feat, angle_stats)
+                        var_graph.edges['struct'].data['dist'] = normalize_feat(dist_feat, dist_stats)
                     var_graph.edges['sca'].data['sca'] = g_sca_sub.edata['sca'].unsqueeze(1)
                     var_graph.edges['dca'].data['dca'] = torch.cat([g_dca_sub.edata['di'].unsqueeze(1),
                                                                       g_dca_sub.edata['mi'].unsqueeze(1)], dim=-1)
@@ -179,7 +187,6 @@ class VariantGraphDataSet(Dataset):
                     var_graph.ndata['ref_aa'] = torch.tensor(seq_array[start_idx:(end_idx+1)], dtype=torch.int64)
                     var_graph.ndata['expasy'] = torch.tensor(feat_data[start_idx: end_idx + 1])
 
-                    # TODO: automatically get all ndata names and concat
                     seq_pos_remain = list(range(start_idx+1, end_idx+2))  # convert index to 1-based sequential positions
                     struct_etype = 'struct'
 
@@ -241,6 +248,8 @@ class VariantGraphDataSet(Dataset):
             self.n_nodes.append(var_graph.num_nodes())
             self.n_edges.append(var_graph.num_edges())
             uprot_prev = uprot
+            struct_prev = struct_id
+            model_prev = model
 
     def __getitem__(self, index):
         graph, label, alt_aa, target_idx, var_id = self.data[index]