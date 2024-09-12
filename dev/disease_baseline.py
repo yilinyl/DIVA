29ccfec68d6c11c1be94eed1561eb6aef7f2d571
yilinyl <yilin9705@gmail.com>
2024-09-12 10:39:39 -0400

    baseline feature extration

---
+++ dev/disease_baseline.py
@@ -0,0 +1,241 @@
+import os, sys
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+sys.path.append(os.path.abspath('..'))
+
+from pathlib import Path
+from dataclasses import dataclass, field
+from typing import Optional
+import yaml
+import json
+import copy
+import argparse
+
+import numpy as np
+import pandas as pd
+
+import torch
+from torch import nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data.dataloader import DataLoader
+from transformers import AutoModel, AutoTokenizer, BertTokenizer, BertModel, BertForMaskedLM, EsmForMaskedLM, AutoModelForMaskedLM
+from transformers.models.bert.modeling_bert import BertOnlyMLMHead
+
+from data.dis_var_dataset import ProteinVariantDatset, ProteinVariantDataCollator, PhenotypeDataset, TextDataCollator
+import logging
+from datetime import datetime
+from utils import str2bool, setup_logger, set_seed, load_input_to_device, _save_scores
+from metrics import *
+from dev.preprocess.utils import parse_fasta_info
+from dev.disease_model_pipeline import load_config, env_setup
+from dev.models.baseline_models import LMBaseModel
+
+
+def extract_text_emb(text_encoder, device, pheno_loader):
+    text_encoder.eval()
+    all_pheno_emb_list = []
+
+    with torch.no_grad():
+        for idx, batch_pheno in enumerate(pheno_loader):
+            # pheno_input_dict = load_input_to_device(batch_pheno, device)
+            pheno_input_dict = batch_pheno.to(device)
+            pheno_embs = text_encoder(
+                pheno_input_dict['input_ids'],
+                attention_mask=pheno_input_dict['attention_mask'],
+                token_type_ids=pheno_input_dict['token_type_ids'],
+                # token_type_ids=torch.zeros(pheno_input_dict['input_ids'].size(), dtype=torch.long, device=self.device),
+                output_attentions=False,
+                output_hidden_states=True,
+                return_dict=None
+                ).hidden_states[-1]  # n_var, max_pos_pheno_length, pheno_emb_dim
+            batch_size = pheno_input_dict['input_ids'].shape[0]
+            pheno_embs = torch.stack([pheno_embs[i, pheno_input_dict['attention_mask'][i, :].bool()].mean(dim=0) for i in range(batch_size)], dim=0)
+            all_pheno_emb_list.append(pheno_embs.detach().cpu().numpy())
+        all_pheno_embs = np.concatenate(all_pheno_emb_list, 0)        
+
+    return all_pheno_embs
+
+
+def compile_features(model, data_loader, device):
+
+    var_emb_list = []
+    var_pheno_label_all = []
+    with torch.no_grad():
+        for batch_idx, batch_data in enumerate(data_loader):
+            seq_feat_dict = load_input_to_device(batch_data['seq_input_feat'], device)
+            desc_feat_dict = load_input_to_device(batch_data['desc_input_feat'], device)
+            variant_data = load_input_to_device(batch_data['variant'], device=device, exclude_keys=['var_names'])
+            
+            seq_pheno_emb_raw, _ = model(seq_feat_dict, variant_data, desc_feat_dict)
+            var_emb_list.append(seq_pheno_emb_raw.detach().cpu().numpy())
+            var_pheno_label_all.extend(variant_data['pos_pheno_idx'].detach().cpu().tolist())
+            # mlm_logits_list.append(mlm_logits.detach().cpu())
+    
+        var_emb_raw = np.concatenate(var_emb_list, 0)
+        
+    return var_emb_raw, var_pheno_label_all
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--config", help="configuration file", default='./configs/dis_var_config_baseline.yaml')
+    parser.add_argument("-c_fmt", "--config_fmt", help="configuration file format", default='yaml')
+    parser.add_argument("-s", "--seed", help="random seed for PyTorch", type=int, default=1024)
+    parser.add_argument('--data_dir', help='Data directory')
+    parser.add_argument('--exp_dir', help='Directory for all training related files, e.g. checkpoints, log')
+    parser.add_argument('--experiment', help='Experiment name')
+    parser.add_argument('--log_level', default='info', help='Log level')
+    # args, unparsed = parser.parse_known_args()
+    args = parser.parse_args()
+
+    return args
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    config = load_config(args.config, format=args.config_fmt)
+
+    config, device = env_setup(args, config, use_timestamp=False)
+
+    data_configs = config['dataset']
+    model_args = config['model']
+
+    exp_dir = config['exp_dir']
+    emb_output_path = Path(exp_dir) / 'embeddings'
+
+    # pheno_result_path = result_path / 'phenotype'
+    if not emb_output_path.exists():
+        emb_output_path.mkdir(parents=True)
+    
+    prot2seq = dict()
+    prot2desc = dict()
+    data_root = Path(data_configs['data_dir'])
+    for fname in data_configs['seq_fasta']:
+        try:
+            seq_dict, desc_dict = parse_fasta_info(fname)
+            prot2seq.update(seq_dict)
+            prot2desc.update(desc_dict)  # string of protein definition E.g. BRCA1_HUMAN Breast cancer type 1 susceptibility protein
+        except FileNotFoundError:
+            pass
+    
+    # prot2desc = dict()
+    for fpath in data_configs['prot_meta_data']:
+        try:
+            df_meta = pd.read_csv(fpath, sep='\t').dropna(subset=['function'])
+            # meta_info_all.append(df)
+            prot_func_dict = dict(zip(df_meta['UniProt'], df_meta['function']))
+            prot2desc.update(prot_func_dict)
+        except FileNotFoundError:
+            pass
+        
+    for prot, desc in prot2desc.items():
+        # update description of isoforms
+        if prot.find('-') >= 0:
+            prot2desc[prot] = ' '.join([desc, prot2desc.get(prot.split('-')[0], '')]).strip()
+    
+    data_configs['seq_dict'] = prot2seq
+    data_configs['protein_info_dict'] = prot2desc
+
+    # Initialize tokenizer
+    protein_tokenizer = AutoTokenizer.from_pretrained(model_args['protein_lm_path'],
+        do_lower_case=False
+    )
+    text_tokenizer = BertTokenizer.from_pretrained(model_args['text_lm_path'])
+
+    # Load data
+    with open(data_configs['phenotype_vocab_file'], 'r') as f:
+        phenotype_vocab = f.read().splitlines()
+    phenotype_vocab.insert(0, text_tokenizer.unk_token)  # add unknown token
+    
+    if data_configs['use_pheno_desc']:
+        with open(data_configs['phenotype_desc_file']) as f:
+            pheno_desc_dict = json.load(f)
+    else:
+        pheno_desc_dict = None
+
+    pheno_dataset = PhenotypeDataset(phenotype_vocab, pheno_desc_dict, use_desc=data_configs['use_pheno_desc'])
+    pheno_collator = TextDataCollator(text_tokenizer, padding=True)
+    phenotype_loader = DataLoader(pheno_dataset, batch_size=config['pheno_batch_size'], collate_fn=pheno_collator, shuffle=False)
+    train_dataset = ProteinVariantDatset(**data_configs, 
+                                         variant_file=data_configs['input_file']['train'], 
+                                         split='train', 
+                                         phenotype_vocab=phenotype_vocab, 
+                                         protein_tokenizer=protein_tokenizer, 
+                                         text_tokenizer=text_tokenizer,
+                                         pheno_desc_dict=pheno_desc_dict,
+                                        #  use_struct_neighbor=data_configs['use_struct_neighbor'],
+                                         access_to_context=True)
+    
+    prot_var_cache = train_dataset.get_protein_cache()
+    val_dataset = ProteinVariantDatset(**data_configs, 
+                                         variant_file=data_configs['input_file']['val'], 
+                                         split='val', 
+                                         phenotype_vocab=phenotype_vocab, 
+                                         protein_tokenizer=protein_tokenizer, 
+                                         text_tokenizer=text_tokenizer,
+                                         pheno_desc_dict=pheno_desc_dict,
+                                         prot_var_cache=prot_var_cache,
+                                         access_to_context=False)  # context variants in validation set not visible to each other
+    
+
+    prot_var_cache = val_dataset.get_protein_cache()
+    
+    test_dataset = ProteinVariantDatset(**data_configs, 
+                                         variant_file=data_configs['input_file']['test'], 
+                                         split='test', 
+                                         phenotype_vocab=phenotype_vocab, 
+                                         protein_tokenizer=protein_tokenizer, 
+                                         text_tokenizer=text_tokenizer,
+                                         pheno_desc_dict=pheno_desc_dict,
+                                        #  var_db=var_db,
+                                         prot_var_cache=prot_var_cache,
+                                         access_to_context=False)
+
+    # Initilize pretrained encoders:
+    # seq_encoder = EsmForMaskedLM.from_pretrained(model_args['protein_lm_path'])
+    seq_encoder = AutoModelForMaskedLM.from_pretrained(model_args['protein_lm_path'])
+    text_encoder = BertForMaskedLM.from_pretrained(model_args['text_lm_path'])
+
+    # Non-shuffle for all...
+    train_collator = ProteinVariantDataCollator(train_dataset.get_protein_data(), protein_tokenizer, text_tokenizer, phenotype_vocab=phenotype_vocab, 
+                                                use_prot_desc=True, max_protein_length=data_configs['max_protein_seq_length'], half_window_size=data_configs['half_window_size'],
+                                                context_agg_opt=data_configs['context_agg_option'], use_pheno_desc=data_configs['use_pheno_desc'], 
+                                                pheno_desc_dict=pheno_desc_dict, use_struct_vocab=data_configs['use_struct_vocab'], 
+                                                use_struct_neighbor=data_configs['use_struct_neighbor'], struct_radius_cutoff=data_configs['struct_radius_cutoff'])
+    train_loader = DataLoader(train_dataset, batch_size=config['batch_size'], collate_fn=train_collator, shuffle=False)
+    val_collator = ProteinVariantDataCollator(val_dataset.get_protein_data(), protein_tokenizer, text_tokenizer, phenotype_vocab=phenotype_vocab, 
+                                              use_prot_desc=True, max_protein_length=data_configs['max_protein_seq_length'], half_window_size=data_configs['half_window_size'],
+                                              context_agg_opt=data_configs['context_agg_option'], use_pheno_desc=data_configs['use_pheno_desc'], 
+                                              pheno_desc_dict=pheno_desc_dict, use_struct_vocab=data_configs['use_struct_vocab'], 
+                                              use_struct_neighbor=data_configs['use_struct_neighbor'], struct_radius_cutoff=data_configs['struct_radius_cutoff'])
+    validation_loader = DataLoader(val_dataset, batch_size=config['batch_size'], collate_fn=val_collator)
+    test_collator = ProteinVariantDataCollator(test_dataset.get_protein_data(), protein_tokenizer, text_tokenizer, phenotype_vocab=phenotype_vocab, 
+                                               use_prot_desc=True, max_protein_length=data_configs['max_protein_seq_length'], half_window_size=data_configs['half_window_size'],
+                                               context_agg_opt=data_configs['context_agg_option'], use_pheno_desc=data_configs['use_pheno_desc'], 
+                                               pheno_desc_dict=pheno_desc_dict, use_struct_vocab=data_configs['use_struct_vocab'], 
+                                               use_struct_neighbor=data_configs['use_struct_neighbor'], struct_radius_cutoff=data_configs['struct_radius_cutoff'])
+    test_loader = DataLoader(test_dataset, batch_size=config['batch_size'], collate_fn=test_collator)
+
+    loader_dict = {'train': train_loader, 'test': test_loader, 'val': validation_loader}
+
+    if model_args['frozen_bert']:
+        # prot_unfreeze_layers = ['esm.encoder.layer.11']
+        for name, parameters in seq_encoder.named_parameters():
+            parameters.requires_grad = False
+        # text_unfreeze_layers = ['bert.encoder.layer.11']
+        for name, parameters in text_encoder.named_parameters():
+            parameters.requires_grad = False
+
+    seq_encoder = seq_encoder.to(device)
+    text_encoder = text_encoder.to(device)
+    base_lm_model = LMBaseModel(seq_encoder, text_encoder, use_desc=True)
+    base_lm_model = base_lm_model.to(device)
+    for split, loader in loader_dict.items():
+        var_embs, var_dis_labels = compile_features(base_lm_model, loader, device=device)
+        np.save(emb_output_path / f'{split}_var_embs.npy', var_embs)
+        with open(emb_output_path / f'{split}_var_pheno_idx.txt', 'w') as f:
+            f.write('\n'.join([str(s) for s in var_dis_labels]))
+
+    all_pheno_embs = extract_text_emb(text_encoder, device, phenotype_loader)
+    np.save(emb_output_path / 'phenotype_emb.npy', all_pheno_embs)
---
+++ dev/models/baseline_models.py
@@ -0,0 +1,101 @@
+import os
+import json
+from typing import Dict, List, Optional, Tuple, Union
+import numpy as np
+import dataclasses
+from dataclasses import dataclass
+import tqdm
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from transformers import BertModel, BertTokenizer
+from transformers import PreTrainedTokenizerBase, PreTrainedModel
+import pandas as pd
+
+from dev.models.protein_encoder import ProteinEncoder
+
+class LMBaseModel(nn.Module):
+    def __init__(self, 
+                 seq_encoder: Union[nn.Module, PreTrainedModel], 
+                 text_encoder: Union[nn.Module, PreTrainedModel],
+                 use_desc: bool = False):
+        super(LMBaseModel, self).__init__()
+
+        self.seq_encoder = seq_encoder
+        self.text_encoder = text_encoder
+        self.use_desc = use_desc
+        self.device = seq_encoder.device
+
+        self.protein_encoder = ProteinEncoder(seq_encoder=seq_encoder,
+                                              text_encoder=text_encoder,
+                                              use_desc=use_desc,
+                                              device=self.device)
+    
+    def forward(self, seq_input_feat, variant_data, desc_input_feat):
+        # seq_outputs = self.seq_encoder(
+        #     seq_input_feat['input_ids'],
+        #     attention_mask=seq_input_feat['attention_mask'],
+        #     # token_type_ids=seq_input_data['token_type_ids'],
+        #     output_attentions=False,
+        #     output_hidden_states=True,
+        # )
+        # # seq_embs = torch.stack([seq_outputs.last_hidden_state[i, attn_mask[i, :], :][1:-1].mean(dim=0) for i in range(num_batch_size)], dim=0)
+        # seq_embs, mlm_logits = seq_outputs.hidden_states[-1], seq_outputs.logits
+
+        # prot_desc_emb = self.text_encoder(
+        #     desc_input_feat['input_ids'],
+        #     attention_mask=desc_input_feat['attention_mask'],
+        #     token_type_ids=torch.zeros(desc_input_feat['input_ids'].size(), dtype=torch.long, device=self.device),
+        #     output_attentions=False,
+        #     output_hidden_states=True,
+        #     return_dict=None
+        # ).hidden_states[-1]
+        seq_embs, mlm_logits, prot_desc_emb = self.protein_encoder(seq_input_feat, desc_input_feat)
+
+        # Pathogenicity prediction
+        desc_emb_agg = torch.stack([prot_desc_emb[i, desc_input_feat['attention_mask'][i, :].bool(), :][0] for i in range(prot_desc_emb.size(0))], dim=0)
+        
+        alt_seq_input_feat = {  # All ALT seq
+            'input_ids': variant_data['var_seq_input_ids'],
+            'attention_mask': seq_input_feat['attention_mask'][variant_data['prot_idx']]
+        }
+        alt_seq_embs = self.protein_encoder.embed_protein_seq(alt_seq_input_feat)
+        alt_seq_embs = torch.stack([alt_seq_embs[i, alt_seq_input_feat['attention_mask'][i, :].bool(), :][0] for i in range(alt_seq_embs.size(0))], dim=0)
+        alt_seq_func_embs = torch.cat([alt_seq_embs, desc_emb_agg[variant_data['prot_idx']]], dim=-1)  # concatenate alt-seq embedding & prot-desc embedding
+
+        max_text_length = self.text_encoder.config.max_position_embeddings
+        pheno_input_ids = variant_data['context_pheno_input_ids']
+        pheno_attn_mask = variant_data['context_pheno_attention_mask']
+        pheno_indices = variant_data['context_pheno_indices']
+        n_uniq_phenos = pheno_input_ids.shape[0]
+        # assert pheno_input_ids.shape[0] == n_pheno_vars
+
+        if pheno_input_ids.shape[-1] > max_text_length:
+            pheno_input_ids = pheno_input_ids[:, :max_text_length]
+            pheno_attn_mask = pheno_attn_mask[:, :max_text_length]
+        
+        seq_context_pheno_emb_raw = self.text_encoder(
+            pheno_input_ids,
+            attention_mask=pheno_attn_mask,
+            token_type_ids=torch.zeros(pheno_input_ids.size(), dtype=torch.long, device=self.device),
+            output_attentions=False,
+            output_hidden_states=True,
+            return_dict=None
+        ).hidden_states[-1]
+
+        seq_context_pheno_emb_raw = torch.stack([seq_context_pheno_emb_raw[i, pheno_attn_mask[i, :].bool(), :][0] for i in range(n_uniq_phenos)], dim=0)
+        seq_pheno_emb_raw = torch.cat([alt_seq_func_embs[variant_data['infer_pheno_vec'].bool()], seq_context_pheno_emb_raw], dim=-1)   # n_var, (seq_emb_dim + desc_emb_dim + pheno_emb_dim)
+
+        return seq_pheno_emb_raw, mlm_logits
+    
+
+    def embed_protein_seq(self, seq_input_feat):
+        seq_embs = self.seq_encoder(
+            seq_input_feat['input_ids'],
+            attention_mask=seq_input_feat['attention_mask'],
+            # token_type_ids=token_type_ids,
+            output_attentions=False,
+            output_hidden_states=True,
+        ).hidden_states[-1]
+
+        return seq_embs
